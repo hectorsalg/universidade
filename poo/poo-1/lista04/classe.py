6488ffdea920d0f69997d2aa88c9f0dc8d569a70
Hector Salgueiros <99827871+hectorsalg@users.noreply.github.com>
2022-09-21 14:28:37 -0300

    finalizei a Lista 04

--- poo/poo-1/lista04/classe.py
+++ poo/poo-1/lista04/classe.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 
 banco = {}
+contas = [0]
+cliente_contas = []
+hist_tributo = []
+
 class Cadastro:
 
     def __init__(self, nome, cpf, dt_nasc):
@@ -22,8 +26,11 @@ class Cliente(Cadastro):
         self._total_corrente = 0
         self._total_poupanca = 0
         self._total_seguros = 0
-        self._total_contas = 0
+        contas[0] += 0
+        banco['Quantidade de Bancos'] = contas
+        banco[self._nome] = [self._total_corrente + self._total_poupanca, self._total_seguros]
         self._hist_trans = []
+        self._count_tributo = 0
             
     def contaCorrente(self, numero, saldo, hist_trans = datetime.now()):
         if self._total_corrente == 0:
@@ -31,8 +38,10 @@ class Cliente(Cadastro):
             self._saldo_corrente = saldo
             self._hist_trans.append(f'Conta Corrente criada: {hist_trans}')
             self._total_corrente += 1
-            self._total_contas += 1
-            banco['Quantidade de Bancos'] = [self._total_contas]
+            cliente_contas.append(self)
+            contas[0] += 1
+            banco['Quantidade de Bancos'] = contas
+            banco[self._nome] = [f'Total de contas: {self._total_corrente + self._total_poupanca}', f' Total de seguros: {self._total_seguros}']
             return True
         else:
             print('Não foi possível criar conta corrente, o usuário já possui!')
@@ -44,16 +53,30 @@ class Cliente(Cadastro):
             self._saldo_poupanca = saldo
             self._hist_trans.append(f'Conta Poupança criada: {hist_trans}')
             self._total_poupanca += 1
-            self._total_contas += 1
-            banco['Quantidade de Bancos'] = [self._total_contas]
+            contas[0] += 1
+            banco['Quantidade de Bancos'] = contas
+            banco[self._nome] = [f'Total de contas: {self._total_corrente + self._total_poupanca}', f' Total de seguros: {self._total_seguros}']
             return True
         else:
             print('Não foi possível criar conta poupança, o usuário já possui!')
             return False
     
+    def tributacao(self):
+        self._tributo = 0
+        if self._total_corrente == 0:
+            self._saldo_corrente = 0
+        if self._total_seguros == 0:
+            self._valor_mensal = 0
+        self._tributo += 10 + (0.01 * self._saldo_corrente) + (0.02 * self._valor_mensal)
+        self._count_tributo += 1
+        hist_tributo.append(f'{self._count_tributo} tributação = {self._tributo}')
+        if self._count_tributo > 1:
+            print(hist_tributo)
+
     def sacar(self, valor):
-        escolha = input('Digite a conta que deseja sacar: ')
-        if escolha == 'corrente':
+        if self._total_corrente == 0 and self._total_poupanca == 0:
+            print('Impossível sacar, nenhuma conta existente!')
+        elif self._total_corrente == 1 and self._total_poupanca == 0:
             if self._saldo_corrente - valor >= 0:
                 self._saldo_corrente -= valor
                 self._hist_trans.append(f'- {valor} : {datetime.now()} na conta corrente')
@@ -61,7 +84,7 @@ class Cliente(Cadastro):
             else:
                 print('Não foi possível realizar saque!')
                 return False
-        elif escolha == 'poupanca' or 'poupança':
+        elif self._total_corrente == 0 and self._total_poupanca == 1:
             if self._saldo_poupanca - valor >= 0:
                 self._saldo_poupanca -= valor
                 self._hist_trans.append(f'- {valor} : {datetime.now()} na conta poupança')
@@ -69,21 +92,51 @@ class Cliente(Cadastro):
             else:
                 print('Não foi possível realizar saque!')
                 return False
+        else:
+            escolha = input('Digite a conta que deseja sacar: ')
+            if escolha == 'corrente':
+                if self._saldo_corrente - valor >= 0:
+                    self._saldo_corrente -= valor
+                    self._hist_trans.append(f'- {valor} : {datetime.now()} na conta corrente')
+                    return True
+                else:
+                    print('Não foi possível realizar saque!')
+                    return False
+            elif escolha == 'poupanca' or 'poupança':
+                if self._saldo_poupanca - valor >= 0:
+                    self._saldo_poupanca -= valor
+                    self._hist_trans.append(f'- {valor} : {datetime.now()} na conta poupança')
+                    return True
+                else:
+                    print('Não foi possível realizar saque!')
+                    return False
     
     def depositar(self, valor):
-        escolha = input('Digite a conta que deseja depositar: ')
-        if escolha == 'corrente':
+        if self._total_corrente == 0 and self._total_poupanca == 0:
+            print('Impossível depositar, nenhuma conta existente!')
+        elif self._total_corrente == 1 and self._total_poupanca == 0:
             self._saldo_corrente += valor
             self._hist_trans.append(f'+ {valor} : {datetime.now()} na conta corrente')
             return True
-        elif escolha == 'poupanca' or 'poupança':
+        elif self._total_corrente == 0 and self._total_poupanca == 1:
             self._saldo_poupanca += valor
             self._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupança')
             return True
+        else:
+            escolha = input('Digite a conta que deseja depositar: ')
+            if escolha == 'corrente':
+                self._saldo_corrente += valor
+                self._hist_trans.append(f'+ {valor} : {datetime.now()} na conta corrente')
+                return True
+            elif escolha == 'poupanca' or 'poupança':
+                self._saldo_poupanca += valor
+                self._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupança')
+                return True
     
     def transferir(self, cliente, valor):
-        escolha = input('Digite a conta que deseja transferir: ')
-        if escolha == 'corrente':
+        if self._total_corrente == 0 and self._total_poupanca == 0:
+            print('Impossível depositar, nenhuma conta existente!')
+        elif self._total_corrente == 1 and self._total_poupanca == 0:
             if self._saldo_corrente - valor >= 0:
                 self._saldo_corrente -= valor
                 self._hist_trans.append(f'- {valor} : {datetime.now()} na conta corrente')
@@ -97,8 +150,7 @@ class Cliente(Cadastro):
             else:
                 print('Não foi possível realizar transferência!')
                 return False
-
-        elif escolha == 'poupanca' or 'poupança':
+        elif self._total_corrente == 0 and self._total_poupanca == 1:
             if self._saldo_poupanca - valor >= 0:
                 self._saldo_poupanca -= valor
                 self._hist_trans.append(f'- {valor} : {datetime.now()} na conta poupança')
@@ -107,19 +159,51 @@ class Cliente(Cadastro):
                     cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta corrente')
                 except:
                     cliente._saldo_poupanca += valor
-                    cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupança')
+                    cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupanca')
                 return True
             else:
                 print('Não foi possível realizar transferência!')
                 return False
+        else:
+            escolha = input('Digite a conta que deseja transferir: ')
+            if escolha == 'corrente':
+                if self._saldo_corrente - valor >= 0:
+                    self._saldo_corrente -= valor
+                    self._hist_trans.append(f'- {valor} : {datetime.now()} na conta corrente')
+                    try:
+                        cliente._saldo_corrente += valor
+                        cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta corrente')
+                    except:
+                        cliente._saldo_poupanca += valor
+                        cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupanca')
+                    return True
+                else:
+                    print('Não foi possível realizar transferência!')
+                    return False
+            elif escolha == 'poupanca' or 'poupança':
+                if self._saldo_poupanca - valor >= 0:
+                    self._saldo_poupanca -= valor
+                    self._hist_trans.append(f'- {valor} : {datetime.now()} na conta poupança')
+                    try:
+                        cliente._saldo_corrente += valor
+                        cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta corrente')
+                    except:
+                        cliente._saldo_poupanca += valor
+                        cliente._hist_trans.append(f'+ {valor} : {datetime.now()} na conta poupança')
+                    return True
+                else:
+                    print('Não foi possível realizar transferência!')
+                    return False
 
     def seguroDeVida(self, valor_mensal, valor_total):
         self._valor_mensal = valor_mensal
         self._valor_total = valor_total
         self._total_seguros += 1
+        banco[self._nome] = [f'Total de contas: {self._total_corrente + self._total_poupanca}', f' Total de seguros: {self._total_seguros}']
     
     def historico(self):
         print(self._hist_trans)
+        return True
 
 class Banco:
 
--- poo/poo-1/lista04/main.py
+++ poo/poo-1/lista04/main.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from classe import *
 
 b = Banco()
@@ -6,12 +5,21 @@ c = Cliente('mel', '123', '11/03/2001', 'front-end')
 c2 = Cliente('hec', '321', '18/12/2002', 'back-end')
 
 c.contaCorrente('465', 0.0)
+# c.contaCorrente('999', 0.0)
 c2.contaCorrente('222', 0.0)
+# c2.contaPoupanca('422', 0.0)
 c.contaPoupanca('678', 0.0)
+c2.seguroDeVida(20, 140)
+
 c2.depositar(10)
 c.depositar(20)
 c.depositar(40)
 c.sacar(15)
 
+c2.tributacao()
+c2.depositar(30)
+# c2.tributacao()
+
 c.historico()
+# c2.historico()
 b.informacoes()
